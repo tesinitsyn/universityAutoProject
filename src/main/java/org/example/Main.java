81eec65643ab3e44d5fe7ca09d9d8b258e8d6bd3
Nikita <nukutac@gmail.com>
2022-09-19 18:58:19 +0300

    UpdateDocument integrated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -10,5 +11,29 @@ public class Main {
         List<String> students = obj.pushToArrayList("C:\\Users\\sitie\\OneDrive\\Рабочий стол\\Пример таблицы.xlsx");
         //delete in future
         System.out.println(students);
+        ArrayList<String> replaceableNames = new ArrayList<>();
+        replaceableNames.add("${instituteName}");
+        replaceableNames.add("${departmentName}");
+        replaceableNames.add("${practiceName}");
+        replaceableNames.add("${orderDate}");
+        replaceableNames.add("${orderName}");
+        replaceableNames.add("${sessionDate}");
+        replaceableNames.add("${studentFN}");
+        replaceableNames.add("${supervisorFN}");
+        replaceableNames.add("${currentYear}");
+        replaceableNames.add("${courseNum}");
+        replaceableNames.add("${groupName}");
+        replaceableNames.add("${studentFullName}");
+        replaceableNames.add("${practicePlaceAndTime}");
+        replaceableNames.add("${position}");
+        replaceableNames.add("${currentDate}");
+        replaceableNames.add("${headOfDFN}");
+        replaceableNames.add("${directionNum}");
+        replaceableNames.add("${directionName}");
+        replaceableNames.add("${profileName}");
+        UpdateDocument objUpdateWord = new UpdateDocument();
+        String inputPath = "D:\\wrdfiles\\Титулы практики.docx";
+        String outputPath = "D:\\wrdfiles\\projecttest.docx";
+        objUpdateWord.updateDocument(inputPath, outputPath, "Морозов", "Солянов");
     }
 }
