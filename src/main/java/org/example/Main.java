420d66346d6beba2e510f58ab0ed5ccab1dfe690
Timofey <sitiev174@gmail.com>
2022-09-19 18:18:57 +0300

    Adding stuff from ExcelParsing module

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ExcelParsing obj = new ExcelParsing();
+        //get arrayList of Students and write file Path
+        List<String> students = obj.pushToArrayList("C:\\Users\\sitie\\OneDrive\\Рабочий стол\\Пример таблицы.xlsx");
+        //delete in future
+        System.out.println(students);
     }
 }
